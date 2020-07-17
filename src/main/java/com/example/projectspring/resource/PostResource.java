fa55daabc01624f3743f63968bb7e61bf8604018
marcoslacroix <marcoslacroix@gmail.com>
2020-07-17 17:44:04 -0300

    Consulta com vários critérios

--- src/main/java/com/example/projectspring/resource/PostResource.java
+++ src/main/java/com/example/projectspring/resource/PostResource.java
@@ -1,19 +1,14 @@
 package com.example.projectspring.resource;
 
 import com.example.projectspring.domain.Post;
-import com.example.projectspring.domain.User;
-import com.example.projectspring.dto.UserDTO;
 import com.example.projectspring.resource.util.URL;
 import com.example.projectspring.service.PostService;
-import com.example.projectspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,16 +17,28 @@ public class PostResource {
     @Autowired
     private PostService service;
 
-    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 
